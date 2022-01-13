267982091b88cfba86a62bc1083c5cbee35ecb13
Trần Thế Anh <53089296+theanhtran372000@users.noreply.github.com>
2022-01-13 14:49:17 +0700

    Update remote.sql

--- remote.sql
+++ remote.sql
@@ -1,3 +1,4 @@
+-- Sử dụng cho cài đặt MySQL từ xa
 CREATE USER 'theanh'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
 GRANT ALL PRIVILEGES ON *.* TO 'theanh'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
